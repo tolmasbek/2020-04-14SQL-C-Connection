1e404ad34c3ef87e3391978dd7cea06995d02b10
tolmasbek <farmonovtt@gmail.com>
2020-04-14 20:00:43 +0500

    ///dfdfdf//

--- Program.cs
+++ Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
-
+using System.Data.SqlClient;
+using System.Data;
 namespace first
 {
     class Program
     {
         static void Main(string[] args)
         {
+            const string constring = @"Data source = TOLMASBEK-ПК; Initial catalog = ContactCenterTaxi; Integrated Security=True"; 
+            SqlConnection con = new SqlConnection(constring);
+            con.Open();
+            if(con.State==ConnectionState.Open)
+            {
+                System.Console.WriteLine("Yes connected !!!");
+            }
+            else
+            {
+                System.Console.WriteLine("No connected !!!");
+            }
 
+            string commandText = "Select *from Operators";
+            SqlCommand command = new SqlCommand(commandText, con);
             
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                System.Console.WriteLine($"Pseudonym:{reader.GetValue(0)},\nCategory:{reader.GetValue(1)},NumberOfCalls:{reader.GetValue(2)}");
+            }
+
+            reader.Close();
+            string insertSqlCommand = string.Format($"insert into Operators([Pseudonym],[Category],[NumberOfCalls]) Values('{"rrrr"}', '{2}', '{345}')");
+            command = new SqlCommand(insertSqlCommand, con);
+            var result = command.ExecuteReader();
+            if(result > 0)
+            {
+                System.Console.WriteLine("Inserted command !!!");
+            }
+
             Console.ReadKey();
         }
     }
