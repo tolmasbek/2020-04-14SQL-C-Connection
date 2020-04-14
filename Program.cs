fb6522de510c021690d31fa448e302a55afc40a4
tolmasbek <farmonovtt@gmail.com>
2020-04-14 19:14:03 +0500

    /////dddd///

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.ReadKey();
+        }
+    }
+}
